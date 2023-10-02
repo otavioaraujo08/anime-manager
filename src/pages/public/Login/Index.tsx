6842f24950d62a17ae8f8c46b637d0d3a513fd06
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-02 17:45:57 -0300

    temp: add button and functions to update name value

--- src/pages/public/Login/Index.tsx
+++ src/pages/public/Login/Index.tsx
@@ -1,12 +1,32 @@
-import { Container, GenericTitle, LoginBox, TextField } from './styles';
+import { useState } from 'react';
+import { Button, Container, GenericTitle, LoginBox, TextField } from './styles';
+import { useDispatch } from 'react-redux';
+import { setName } from '../../../redux/features/nameSlice';
 
 export const Login = () => {
+    const [username, setUsername] = useState<string>('');
+    const dispatch = useDispatch();
+
+    const changeUserName = (name: string) => {
+        setUsername(name);
+    };
+
+    const handleSaveName = () => {
+        dispatch(setName(username));
+    };
+
     return (
         <Container>
             <LoginBox>
                 <GenericTitle>Digite seu nome abaixo</GenericTitle>
 
-                <TextField placeholder="Digite seu nome aqui..." />
+                <TextField
+                    value={username}
+                    placeholder="Digite seu nome aqui..."
+                    onChange={(event) => changeUserName(event.target.value)}
+                />
+
+                <Button onClick={handleSaveName}>Finalizar Login</Button>
             </LoginBox>
         </Container>
     );
