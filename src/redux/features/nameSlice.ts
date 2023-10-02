47678ecf62dd8d35d1d645cfbeac24f6ac580032
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-02 17:45:00 -0300

    feat: create name slice

---
+++ src/redux/features/nameSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface NameState {
+    name: string;
+}
+
+const initialState: NameState = {
+    name: '',
+};
+
+const nameSlice = createSlice({
+    name: 'name',
+    initialState,
+    reducers: {
+        setName: (state, action: PayloadAction<string>) => {
+            state.name = action.payload;
+        },
+        resetName: (state) => {
+            state.name = '';
+        },
+    },
+});
+
+export const { setName, resetName } = nameSlice.actions;
+export default nameSlice.reducer;
