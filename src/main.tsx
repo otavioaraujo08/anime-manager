81299fdf5deb113198a1e9880d39efe42fcf905e
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-02 15:45:44 -0300

    fix: update routes to router provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { router } from './route/routes';
+import { RouterProvider } from 'react-router-dom';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <>
-      ola mundo
-    </>
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
