a8357d857e4b77331d9c6e3013d1479458e959ea
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-02 17:46:20 -0300

    feat: add provider to main DOM

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { router } from './route/routes';
 import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+    <Provider store={store}>
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
+    </Provider>
 );
