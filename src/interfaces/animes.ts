038410ce436439cb10746f52a87b1a947c974383
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-04 10:52:32 -0300

    feat: create anime data interface

---
+++ src/interfaces/animes.ts
@@ -0,0 +1,10 @@
+export interface AnimeData {
+    id: number;
+    title: string;
+    photo: string;
+    DayOfWeek: string;
+    LastDayWatched: string;
+    EpisodesWatched: number;
+    progress: string;
+    season: string;
+}
