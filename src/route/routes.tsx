7329350c598aa656311ab778c0e54329744962e4
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-04 10:51:55 -0300

    feat: add home route

--- src/route/routes.tsx
+++ src/route/routes.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Login } from '../pages/public/Login/Index';
+import { Home } from '../pages/private/home';
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Login />,
+    },
+    {
+        path: '/home',
+        element: <Home />,
     },
 ]);
