5ab7eeae312fdfde8e0e833591cade457c29a36c
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-04 10:57:21 -0300

    feat: update login page logic and styles

--- src/pages/public/Login/Index.tsx
+++ src/pages/public/Login/Index.tsx
@@ -1,32 +1,141 @@
 import { useState } from 'react';
-import { Button, Container, GenericTitle, LoginBox, TextField } from './styles';
+import {
+    Button,
+    Container,
+    GenericSubtitle,
+    GenericTitle,
+    LoginBox,
+    TextField,
+} from './styles';
 import { useDispatch } from 'react-redux';
 import { setName } from '../../../redux/features/nameSlice';
+import { userService } from '../../../services/user';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
     const [username, setUsername] = useState<string>('');
+    const [isLogin, setIsLogin] = useState({
+        login: false,
+        register: false,
+        userChoosen: false,
+    });
     const dispatch = useDispatch();
+    const userWantLogin = isLogin.login;
+    const navigate = useNavigate();
 
     const changeUserName = (name: string) => {
         setUsername(name);
     };
 
+    const changeLoginOption = (login: boolean) => {
+        if (login) {
+            return setIsLogin({
+                login: true,
+                register: false,
+                userChoosen: true,
+            });
+        }
+
+        setIsLogin({
+            login: false,
+            register: true,
+            userChoosen: true,
+        });
+    };
+
     const handleSaveName = () => {
         dispatch(setName(username));
+
+        isLogin.login ? handleGetUserInfos() : handleRegisterInfos();
+    };
+
+    const handleGetUserInfos = async () => {
+        try {
+            const response = await userService.getUsersInfo();
+
+            const filteredUser = response.filter(
+                (user) => user.name === username
+            );
+
+            filteredUser.length
+                ? navigate('/home', { state: filteredUser })
+                : alert('Usuário não encontrado');
+        } catch (err) {
+            alert(err);
+        }
+    };
+
+    const handleRegisterInfos = async () => {
+        try {
+            const response = await userService.postUserInfo(username);
+
+            navigate('/home', { state: response });
+        } catch (err) {
+            alert(err);
+        }
     };
 
     return (
         <Container>
             <LoginBox>
-                <GenericTitle>Digite seu nome abaixo</GenericTitle>
+                {isLogin.userChoosen ? (
+                    <>
+                        <GenericTitle>
+                            {userWantLogin ? 'Login' : 'Cadastro'}
+                        </GenericTitle>
 
-                <TextField
-                    value={username}
-                    placeholder="Digite seu nome aqui..."
-                    onChange={(event) => changeUserName(event.target.value)}
-                />
+                        <GenericSubtitle>
+                            Digite seu nome abaixo
+                        </GenericSubtitle>
 
-                <Button onClick={handleSaveName}>Finalizar Login</Button>
+                        <TextField
+                            value={username}
+                            placeholder="Digite seu nome aqui..."
+                            onChange={(event) =>
+                                changeUserName(event.target.value)
+                            }
+                        />
+
+                        <>
+                            <Button onClick={handleSaveName}>
+                                Finalizar {userWantLogin ? 'Login' : 'Cadastro'}
+                            </Button>
+
+                            <p
+                                style={{
+                                    fontSize: '1rem',
+                                    fontWeight: 600,
+                                    color: '#000000',
+                                    cursor: 'pointer',
+                                    borderBottom: '1px solid #000000',
+                                }}
+                                onClick={() => {
+                                    setIsLogin({
+                                        login: false,
+                                        register: false,
+                                        userChoosen: false,
+                                    });
+                                }}
+                            >
+                                Retornar
+                            </p>
+                        </>
+                    </>
+                ) : (
+                    <>
+                        <GenericTitle>
+                            Escolha como deseja prosseguir
+                        </GenericTitle>
+
+                        <Button onClick={() => changeLoginOption(true)}>
+                            Realizar login
+                        </Button>
+
+                        <Button onClick={() => changeLoginOption(false)}>
+                            Cadastrar
+                        </Button>
+                    </>
+                )}
             </LoginBox>
         </Container>
     );
