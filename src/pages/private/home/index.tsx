2ab17f6521e5570df03371d6a1f09965a7952d20
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-06 14:49:09 -0300

    feat: update home page and add modal component

--- src/pages/private/home/index.tsx
+++ src/pages/private/home/index.tsx
@@ -14,14 +14,31 @@ import {
 } from './styles';
 import { useState, useEffect } from 'react';
 import { AnimeData } from '../../../interfaces/animes';
+import { Modal } from '../../../components/Modal';
+
+interface IAnimeData {
+    isModalOpen: boolean;
+    animeData: AnimeData | {};
+}
 
 export const Home = () => {
+    const [animeData, setAnimeData] = useState<IAnimeData>({
+        isModalOpen: false,
+        animeData: {},
+    });
     const [animeList, setAnimeList] = useState<AnimeData[]>();
     const [searchFilter, setSearchFilter] = useState<string>('');
     const { state } = useLocation();
 
     const handleChangeFilter = (animeName: string) => {
         return setSearchFilter(animeName);
+    };
+
+    const handleChangeModalStatus = (animeInfos?: AnimeData) => {
+        setAnimeData({
+            isModalOpen: !animeData.isModalOpen,
+            animeData: animeInfos || {},
+        });
     };
 
     useEffect(() => {
@@ -33,31 +50,52 @@ export const Home = () => {
     }, [searchFilter, state]);
 
     return (
-        <Container>
-            <Header>
-                <GenericTitle>
-                    Seja bem vindo <strong>{state?.name || 'Usuário X'}</strong>
-                </GenericTitle>
+        <>
+            <Container>
+                <Header>
+                    <GenericTitle>
+                        Seja bem vindo{' '}
+                        <strong>{state?.name || 'Usuário X'}</strong>
+                    </GenericTitle>
 
-                <Textfield
-                    placeholder="Pesquise pelo nome do anime aqui..."
-                    onChange={(event) => handleChangeFilter(event.target.value)}
-                />
-            </Header>
+                    <Textfield
+                        placeholder="Pesquise pelo nome do anime aqui..."
+                        onChange={(event) =>
+                            handleChangeFilter(event.target.value)
+                        }
+                    />
+                </Header>
 
-            <Body>
-                <GenericSubtitle>Animes</GenericSubtitle>
+                <Body>
+                    <GenericSubtitle>Animes</GenericSubtitle>
 
-                <AnimesBox>
-                    {animeList?.map((anime: AnimeData) => (
-                        <AnimeBox key={anime.id}>
-                            <AnimeImage src={anime.photo} alt={anime.title} />
-                            <AnimeTitle>{anime.title}</AnimeTitle>
-                            <AnimeSubtitle>{anime.progress}</AnimeSubtitle>
-                        </AnimeBox>
-                    ))}
-                </AnimesBox>
-            </Body>
-        </Container>
+                    <AnimesBox>
+                        {animeList?.map((anime: AnimeData) => (
+                            <AnimeBox
+                                key={anime.id}
+                                onClick={() => handleChangeModalStatus(anime)}
+                            >
+                                <AnimeImage
+                                    src={anime.photo}
+                                    alt={anime.title}
+                                    loading="lazy"
+                                />
+
+                                <AnimeTitle>{anime.title}</AnimeTitle>
+                                <AnimeSubtitle>{anime.progress}</AnimeSubtitle>
+                            </AnimeBox>
+                        ))}
+                    </AnimesBox>
+                </Body>
+            </Container>
+
+            <Modal
+                title="Informações"
+                isOpen={animeData.isModalOpen}
+                closeModal={handleChangeModalStatus}
+            >
+                <h1>OLA MUNDO</h1>
+            </Modal>
+        </>
     );
 };
