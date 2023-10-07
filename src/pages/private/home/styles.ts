d1f66112e0a239613918435b5b9d7453ab410a48
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-07 20:47:13 -0300

    fix: update file format

--- src/pages/private/home/styles.ts
+++ src/pages/private/home/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import homeWallpaper from '../../../assets/homeWallpaper.png';
+import homeWallpaper from '../../../assets/homeWallpaper.jpg';
 
 export const Container = styled.div`
     height: 100%;
