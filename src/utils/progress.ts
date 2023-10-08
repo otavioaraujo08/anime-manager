b21eeb2d667b53bb0d2b231d310488162350d9ee
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-07 23:37:40 -0300

    feat: create progress list

---
+++ src/utils/progress.ts
@@ -0,0 +1,18 @@
+export const progress = [
+    {
+        name: 'Não iniciado',
+        value: 'Não iniciado',
+    },
+    {
+        name: 'Em andamento',
+        value: 'Em andamento',
+    },
+    {
+        name: 'Pausado',
+        value: 'Pausado',
+    },
+    {
+        name: 'Completo',
+        value: 'Completo',
+    },
+];
