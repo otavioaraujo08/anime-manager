bbfa70671fb6cbfad1fd8ec52ff7002186757618
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-08 19:06:22 -0300

    feat: create seasons list

---
+++ src/utils/season.ts
@@ -0,0 +1,18 @@
+export const seasons = [
+    {
+        name: 'Verão',
+        value: 'Verão',
+    },
+    {
+        name: 'Outono',
+        value: 'Outono',
+    },
+    {
+        name: 'Inverno',
+        value: 'Inverno',
+    },
+    {
+        name: 'Primavera',
+        value: 'Primavera',
+    },
+];
