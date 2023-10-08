df168c2210dfc5eabd717db3bfee1dfaef4bac64
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-07 23:38:17 -0300

    fix: add params on GET animes function

--- src/services/anime.ts
+++ src/services/anime.ts
@@ -1,11 +1,18 @@
 import { AnimeData } from '../interfaces/animes';
 import { api as apiService, ApiService, defaultUrl } from './api';
 
+interface getAnimesInfoProps {
+    userId?: number;
+    id?: number;
+}
+
 class AnimeService {
     constructor(private readonly api: ApiService) {}
 
-    public getAnimesInfo = async (userId: number): Promise<AnimeData[]> => {
-        return this.api.get(`${defaultUrl}/animes?userId=${userId}`);
+    public getAnimesInfo = async (
+        params: getAnimesInfoProps
+    ): Promise<AnimeData[]> => {
+        return this.api.get(`${defaultUrl}/animes`, { params });
     };
 }
 export const animeService = new AnimeService(apiService);
