158d68f8ccc50f5a037b36e51c142993b09e2160
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-08 20:02:05 -0300

    feat: create anime infos page

---
+++ src/pages/private/createAnime/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export const CreateAnime = () => {
+    return (
+        <Container>
+            <h1>Create anime infos page</h1>
+        </Container>
+    );
+};
