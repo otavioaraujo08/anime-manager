8e9b0d35625b0889a5ab12af3fd691e577adbf91
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-07 23:37:51 -0300

    feat: create days of week list

---
+++ src/utils/daysOfWeek.ts
@@ -0,0 +1,30 @@
+export const daysOfWeek = [
+    {
+        name: 'Segunda',
+        value: 'Segunda',
+    },
+    {
+        name: 'Terça',
+        value: 'Terça',
+    },
+    {
+        name: 'Quarta',
+        value: 'Quarta',
+    },
+    {
+        name: 'Quinta',
+        value: 'Quinta',
+    },
+    {
+        name: 'Sexta',
+        value: 'Sexta',
+    },
+    {
+        name: 'Sábado',
+        value: 'Sábado',
+    },
+    {
+        name: 'Domingo',
+        value: 'Domingo',
+    },
+];
