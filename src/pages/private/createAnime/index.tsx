853dbe777831b45a55a4b8e1daffc4a17d173dad
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-10 12:35:41 -0300

    feat: create branding side of page

--- src/pages/private/createAnime/index.tsx
+++ src/pages/private/createAnime/index.tsx
@@ -1,9 +1,46 @@
-import { Container } from './styles';
+import logo from '../../../assets/logo.png';
+import megumin from '../../../assets/megumin.png';
+import {
+    AnimeImage,
+    Body,
+    BodySubtitle,
+    BodyText,
+    BodyTitle,
+    BrandingContainer,
+    Container,
+    FormContiainer,
+    Header,
+    HeaderTitle,
+    LogoImage,
+} from './styles';
 
 export const CreateAnime = () => {
     return (
         <Container>
-            <h1>Create anime infos page</h1>
+            <BrandingContainer>
+                <Header>
+                    <LogoImage src={logo} alt="anime logo" loading="lazy" />
+
+                    <HeaderTitle>AniManager</HeaderTitle>
+                </Header>
+
+                <Body>
+                    <AnimeImage src={megumin} alt="anime logo" loading="lazy" />
+
+                    <BodyText>
+                        <BodyTitle>Ajudando a gerenciar seus animes</BodyTitle>
+
+                        <BodySubtitle>
+                            Adicione as informações do seu anime favorito e
+                            contribua para um acompanhamento diário dos
+                            episódios da temporada
+                        </BodySubtitle>
+                    </BodyText>
+                </Body>
+            </BrandingContainer>
+            <FormContiainer>
+                <h1>Adicionar informações</h1>
+            </FormContiainer>
         </Container>
     );
 };
