f68e2c62c0c792345c4fba02dc4b594c32055b22
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-10 11:51:07 -0300

    fix: remove background

--- src/pages/private/createAnime/styles.ts
+++ src/pages/private/createAnime/styles.ts
@@ -1,10 +1,6 @@
 import styled from 'styled-components';
-import homeWallpaper from '../../../assets/homeWallpaper.jpg';
 
 export const Container = styled.div`
     height: 100%;
     min-height: 100vh;
-    background-image: url(${homeWallpaper});
-    background-repeat: no-repeat;
-    background-size: cover;
 `;
