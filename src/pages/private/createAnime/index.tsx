b32f9bec5aafd6ad4e2fe44c0a37c5d96223bda5
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-11 15:35:57 -0300

    feat: create area to add anime infos

--- src/pages/private/createAnime/index.tsx
+++ src/pages/private/createAnime/index.tsx
@@ -1,5 +1,10 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
 import logo from '../../../assets/logo.png';
 import megumin from '../../../assets/megumin.png';
+import { WarningText } from '../../../components/WarningText';
+import { daysOfWeek } from '../../../utils/daysOfWeek';
+import { progress } from '../../../utils/progress';
+import { seasons } from '../../../utils/season';
 import {
     AnimeImage,
     Body,
@@ -7,15 +12,59 @@ import {
     BodyText,
     BodyTitle,
     BrandingContainer,
+    Button,
     Container,
     Form,
     FormContiainer,
     Header,
     HeaderTitle,
     LogoImage,
+    TextFieldStyled,
+    SelectFieldStyled,
 } from './styles';
+import { animeService } from '../../../services/anime';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+type IFormInput = {
+    title: string;
+    photo: string;
+    dayOfWeek: string;
+    lastDayWatched: string;
+    episodesWatched: number;
+    progress: string;
+    season: string;
+};
 
 export const CreateAnime = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IFormInput>();
+    const { state } = useLocation();
+    const navigate = useNavigate();
+
+    const handleRedirectPage = () => {
+        navigate('/home', {
+            state: state,
+        });
+    };
+
+    const handlePostAnimeInfos: SubmitHandler<IFormInput> = async (
+        data: IFormInput
+    ) => {
+        try {
+            await animeService.createAnimeInfos({
+                ...data,
+                userId: state.id,
+            });
+
+            handleRedirectPage();
+        } catch (error: any) {
+            alert(error.message);
+        }
+    };
+
     return (
         <Container>
             <BrandingContainer>
@@ -46,12 +95,127 @@ export const CreateAnime = () => {
                         Adicionar informações
                     </HeaderTitle>
                     <BodySubtitle $branding={false}>
-                        Adicione as informações necessárias para salvar os dados
+                        Preencha as informações necessárias para salvar os dados
                         do seu anime.
                     </BodySubtitle>
                 </Header>
 
-                <Form></Form>
+                <Form onSubmit={handleSubmit(handlePostAnimeInfos)}>
+                    <div>
+                        <h1>Nome</h1>
+
+                        <TextFieldStyled
+                            placeholder="Digite o nome aqui..."
+                            {...register('title', { required: true })}
+                        />
+
+                        {errors.title && (
+                            <WarningText>Campo obrigatório !</WarningText>
+                        )}
+                    </div>
+
+                    <div>
+                        <h1>Foto</h1>
+
+                        <TextFieldStyled
+                            placeholder="Insira a URL aqui..."
+                            type="url"
+                            {...register('photo', { required: true })}
+                        />
+
+                        {errors.photo && (
+                            <WarningText>Campo obrigatório !</WarningText>
+                        )}
+                    </div>
+
+                    <div>
+                        <h1>Dia de exibição</h1>
+
+                        <SelectFieldStyled
+                            {...register('dayOfWeek', { required: true })}
+                        >
+                            {daysOfWeek.map((day) => (
+                                <option key={day.value} value={day.value}>
+                                    {day.name}
+                                </option>
+                            ))}
+                        </SelectFieldStyled>
+
+                        {errors.dayOfWeek && (
+                            <WarningText>Campo obrigatório !</WarningText>
+                        )}
+                    </div>
+
+                    <div>
+                        <h1>Última exibição</h1>
+
+                        <TextFieldStyled
+                            type="date"
+                            {...register('lastDayWatched', { required: true })}
+                        />
+
+                        {errors.lastDayWatched && (
+                            <WarningText>Campo obrigatório !</WarningText>
+                        )}
+                    </div>
+
+                    <div>
+                        <h1>Episódios assistidos</h1>
+
+                        <TextFieldStyled
+                            {...register('episodesWatched', { required: true })}
+                            placeholder="Digite a quantidade aqui"
+                            type="number"
+                        />
+
+                        {errors.episodesWatched && (
+                            <WarningText>Campo obrigatório !</WarningText>
+                        )}
+                    </div>
+
+                    <div>
+                        <h1>Progresso</h1>
+
+                        <SelectFieldStyled
+                            {...register('progress', { required: true })}
+                        >
+                            {progress.map((progress) => (
+                                <option
+                                    key={progress.value}
+                                    value={progress.value}
+                                >
+                                    {progress.name}
+                                </option>
+                            ))}
+                        </SelectFieldStyled>
+
+                        {errors.progress && (
+                            <WarningText>Campo obrigatório !</WarningText>
+                        )}
+                    </div>
+
+                    <div>
+                        <h1>Temporada</h1>
+
+                        <SelectFieldStyled
+                            {...register('season', { required: true })}
+                        >
+                            {seasons.map((season) => (
+                                <option key={season.value} value={season.value}>
+                                    {season.name}
+                                </option>
+                            ))}
+                        </SelectFieldStyled>
+
+                        {errors.season && (
+                            <WarningText>Campo obrigatório !</WarningText>
+                        )}
+                    </div>
+
+                    <Body>
+                        <Button type="submit">Salvar</Button>
+                    </Body>
+                </Form>
             </FormContiainer>
         </Container>
     );
