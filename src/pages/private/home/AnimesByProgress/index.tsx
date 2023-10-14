416503cfc16a5433821ad224e2c9ac2ea39f0395
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-13 22:57:28 -0300

    feat: update animes by progress styles and logic

--- src/pages/private/home/AnimesByProgress/index.tsx
+++ src/pages/private/home/AnimesByProgress/index.tsx
@@ -1,16 +1,54 @@
 import { AnimeData } from '../../../../interfaces/animes';
+import {
+    AnimeDiv,
+    AnimesDiv,
+    AnimeImage,
+    AnimeInfo,
+    Container,
+    Label,
+    Title,
+} from './styles';
 
 interface AnimesByProgressProps {
+    progress: 'Não iniciado' | 'Em andamento' | 'Pausado' | 'Completo';
     animeList: AnimeData[];
 }
 
-export const AnimesByProgress = ({ animeList }: AnimesByProgressProps) => {
-    console.log(animeList);
-
-    return (
-        <>
-            <h1>ola</h1>
-            <p>mundo</p>
-        </>
+export const AnimesByProgress = ({
+    progress,
+    animeList,
+}: AnimesByProgressProps) => {
+    const filteredAnimeList = animeList.filter(
+        (anime) => anime.progress === progress
     );
+
+    return filteredAnimeList.length ? (
+        <Container>
+            <Title>
+                Animes ordenados p/ progresso: <strong>{progress}</strong>
+            </Title>
+
+            <AnimesDiv>
+                {filteredAnimeList.map((anime) => (
+                    <AnimeDiv key={anime.id}>
+                        <AnimeImage alt={anime.title} src={anime.photo} />
+
+                        <div>
+                            <Label>Título:</Label>
+                            <AnimeInfo>{anime.title}</AnimeInfo>
+
+                            <Label>Dia da semana:</Label>
+                            <AnimeInfo>{anime.dayOfWeek}</AnimeInfo>
+
+                            <Label>Episódios concluídos:</Label>
+                            <AnimeInfo>{anime.episodesWatched}</AnimeInfo>
+
+                            <Label>Último dia concluído:</Label>
+                            <AnimeInfo>{anime.lastDayWatched}</AnimeInfo>
+                        </div>
+                    </AnimeDiv>
+                ))}
+            </AnimesDiv>
+        </Container>
+    ) : null;
 };
