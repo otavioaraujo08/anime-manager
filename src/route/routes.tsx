e39885f78f96a78f9da2cc470e7f5cd70d2d4c98
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-21 11:44:17 -0300

    fix: update folder name in route

--- src/route/routes.tsx
+++ src/route/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Home } from '../pages/private/home';
 import { CreateAnime } from '../pages/private/createAnime';
-import { Login } from '../pages/public/Login';
+import { Login } from '../pages/public/login';
 
 export const router = createBrowserRouter([
     {
