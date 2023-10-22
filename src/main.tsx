6628098b919ec3264dfb026b0b4f1294f501784c
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-22 20:44:11 -0300

    feat: trying a hotfix for aliase problem

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { createBrowserRouter } from 'react-router-dom';
 
-import { Login } from './pages/public/login';
+import { Login } from './pages/public/Login';
 import { Home } from './pages/private/home';
 import { CreateAnime } from './pages/private/createAnime';
 
