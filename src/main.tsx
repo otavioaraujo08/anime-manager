3cdb68dbe4f22f508137a0d4ee49a5b5e35fab52
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-10-22 20:20:40 -0300

    feat: update route config to home

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { router } from './route/routes';
 import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import { store } from './redux/store';
+import { createBrowserRouter } from 'react-router-dom';
+import { Login } from '@pages/public/login';
+import { Home } from '@pages/private/home';
+import { CreateAnime } from '@pages/private/createAnime';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Login />,
+    },
+    {
+        path: '/home',
+        element: <Home />,
+    },
+    {
+        path: '/create-anime-info',
+        element: <CreateAnime />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
