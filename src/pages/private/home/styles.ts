2ef7a5ec3e4a67c9775f687448399940ce95fbae
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-01-03 14:56:18 -0300

    fix: update unused styles

--- src/pages/private/home/styles.ts
+++ src/pages/private/home/styles.ts
@@ -77,48 +77,6 @@ export const Body = styled.div`
     gap: 3rem;
 `;
 
-export const BodyWithoutAnimes = styled.div`
-    height: 80vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 2rem;
-`;
-
-export const GenericSubtitle = styled.h2`
-    font-family: 'Poppins', sans-serif;
-    font-size: 1.6rem;
-    font-weight: 500;
-    color: #e5e5e5;
-    letter-spacing: 0.1rem;
-    margin-bottom: 1rem;
-
-    @media (max-width: 800px) {
-        font-size: 1.4rem;
-    }
-
-    @media (max-width: 600px) {
-        font-size: 1.2rem;
-    }
-`;
-
-export const ButtonAddAnimes = styled.button`
-    height: 3rem;
-    width: 15rem;
-    border: none;
-    border-radius: 2%;
-    background: #fca311;
-    color: #fff;
-    cursor: pointer;
-    transition: 2s;
-    font-size: 1.1rem;
-
-    &:hover {
-        background: #b97507;
-    }
-`;
-
 export const AnimesBox = styled.div`
     display: flex;
     flex-wrap: wrap;
