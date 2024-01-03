ad789570e0a3830fac050b42232905584bff955a
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-01-03 13:50:57 -0300

    feat: create Title component

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,38 @@
+import { FaGripLinesVertical } from 'react-icons/fa';
+import styled from 'styled-components';
+
+const TitleContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    margin-bottom: 2rem;
+`;
+
+const TitleStyled = styled.h1`
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.6rem;
+    font-weight: 500;
+    color: #e5e5e5;
+    letter-spacing: 0.1rem;
+
+    @media (max-width: 800px) {
+        font-size: 1.4rem;
+    }
+
+    @media (max-width: 600px) {
+        font-size: 0.8rem;
+    }
+`;
+
+interface TitleProps {
+    title: string;
+}
+
+export const Title = ({ title }: TitleProps) => {
+    return (
+        <TitleContainer>
+            <FaGripLinesVertical size={30} color="#f4d4d4" />
+            <TitleStyled>{title}</TitleStyled>
+        </TitleContainer>
+    );
+};
