960c84e6fb8bf8b86ea06e08b6d93b5165ad8c95
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-06-20 16:52:58 -0300

    feat: create anime view page logic and styles

---
+++ src/pages/private/animePage/index.tsx
@@ -0,0 +1,64 @@
+import { useLocation } from 'react-router-dom';
+import {
+    AnimeBoxContent,
+    AnimeBoxHeader,
+    AnimeBoxContainer,
+    AnimeImage,
+    BoxContentImage,
+    HeaderText,
+    AnimeInfos,
+} from './styles';
+import { useState } from 'react';
+import { AnimeData } from '@interfaces/animes';
+import { AnimeView } from './AnimeView';
+import { AnimeEdit } from './AnimeEdit';
+
+export const AnimePage = () => {
+    const { state } = useLocation();
+    const {
+        title,
+        photo,
+        dayOfWeek,
+        episodesWatched,
+        progress,
+        season,
+    }: AnimeData = state;
+    const [isEdit, setIsEdit] = useState<boolean>(false);
+
+    const handleEditAnime = () => {
+        setIsEdit((state) => !state);
+    };
+
+    console.log(state);
+
+    return (
+        <AnimeBoxContent>
+            <AnimeBoxContainer>
+                <AnimeBoxHeader>
+                    <HeaderText $isEdit={!isEdit} onClick={handleEditAnime}>
+                        Visualizar
+                    </HeaderText>
+                    <HeaderText $isEdit={isEdit} onClick={handleEditAnime}>
+                        Editar
+                    </HeaderText>
+                </AnimeBoxHeader>
+                <AnimeInfos>
+                    {isEdit ? (
+                        <AnimeEdit />
+                    ) : (
+                        <AnimeView
+                            title={title}
+                            dayOfWeek={dayOfWeek}
+                            episodesWatched={episodesWatched}
+                            progress={progress}
+                            season={season}
+                        />
+                    )}
+                </AnimeInfos>
+            </AnimeBoxContainer>
+            <BoxContentImage>
+                <AnimeImage src={photo} alt={title} />
+            </BoxContentImage>
+        </AnimeBoxContent>
+    );
+};
